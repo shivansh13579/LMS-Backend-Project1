f456fc9109d136346fa2c8001b8b69e7a741df12
shivansh13579 <shivansh0975@gmail.com>
2024-03-12 15:34:37 +0530

    create course model

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -17,7 +17,23 @@ const getAllCourses = async function(req,res,next) {
 }
 
 const getLecturesByCourseId = async function(req,res,next){
+      try {
+        const {id} = req.params;
 
+        const course = await Course.findById(id);
+
+        if(!course) {
+            return next(new AppError(e.message,400))
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Course lectures fetched successfully",
+            lectures: course.lectures
+        })
+      } catch (e) {
+         return next(new AppError(e.message,500))
+      }
 }
 
 export {
