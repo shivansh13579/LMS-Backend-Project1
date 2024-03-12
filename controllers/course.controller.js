4133d45d0658f98fba115892df1022184b3f870f
shivansh13579 <shivansh0975@gmail.com>
2024-03-12 22:38:16 +0530

    create coureController

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -1,5 +1,7 @@
 import Course from "../models/course.model.js";
 import AppError from "../utils/error.util.js";
+import cloudinary from 'cloudinary';
+import fs from 'fs/promises';
 
 
 const getAllCourses = async function(req,res,next) {
@@ -23,7 +25,7 @@ const getLecturesByCourseId = async function(req,res,next){
         const course = await Course.findById(id);
 
         if(!course) {
-            return next(new AppError(e.message,400))
+            return next(new AppError("Invalid course id",400))
         }
 
         res.status(200).json({
@@ -36,7 +38,58 @@ const getLecturesByCourseId = async function(req,res,next){
       }
 }
 
+const createCourse = async(req,res,next) => {
+    const {title,description,category,createdBy} = req.body;
+
+    if(!title || !description || !category || !createdBy) {
+        return next(new AppError('All fields are required',400))
+    }
+
+    const course = await Course.create({
+        title,
+        description,
+        category,
+        createdBy,
+    });
+
+    if (!course) {
+        return next(new AppError('Course could not created, please try again',500))
+    }
+
+    if (req.file) {
+        const result = await cloudinary.v2.uploader.upload(req.file.path,{
+            folder: "lms"
+        });
+        if(result) {
+            course.thumbnail.public_id = result.public_id;
+            course.thumbnail.secure_url = result.secure_url;
+        }
+
+        fs.rm(`uploads/${req.file.filename}`);
+    }
+
+    await course.save();
+
+    res.status(200).json({
+        success: true,
+        message: "Course created successfully",
+        course,
+    });
+
+}
+
+const updateCourse = async(req,res,next) => {
+      
+}
+
+const removeCourse = async (req,res,next) => {
+       
+}
+
 export {
     getAllCourses,
-    getLecturesByCourseId
+    getLecturesByCourseId,
+    createCourse,
+    updateCourse,
+    removeCourse
 }
